30e1b4079625d2f6d8c98fd7cd3ff8cfd543c912
Lap-AlfonsoM <alfonso.moreno@dmesoluciones.com>
2018-07-11 09:35:49 -0600

    Primera version del modulo concredito

---
+++ Concredito/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Alfonso Moreno
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Concredito',
+    'description': """
+        Modulo para prestamos concredito""",
+    'version': '1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Alfonso Moreno',
+    'depends': [
+    ],
+    'data': [
+        'security/prestamos.xml',
+        'views/prestamos.xml',
+    ],
+    'demo': [
+        'demo/prestamos.xml',
+    ],
+}
---
+++ Concredito/models/prestamos.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Alfonso Moreno
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, fields, models, _
+
+
+class Prestamos(models.Model):
+
+    _inherit = 'prestamos'
